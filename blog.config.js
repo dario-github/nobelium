a528451ff385fd2412e7de67006b781ebbd072e6
dario-github <zdclink@gmail.com>
2022-07-07 18:33:36 +0000

    [CONF] Change conf

--- blog.config.js
+++ blog.config.js
@@ -1,5 +1,5 @@
 const BLOG = {
-  title: 'Dario Homepage',
+  title: 'Dario Blog',
   author: 'Dario Zhang',
   email: 'donoghzhang@gmail.com',
   link: 'https://dariolink.ga',
@@ -11,11 +11,11 @@ const BLOG = {
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2022, // If leave this empty, current year will be used.
-  postsPerPage: 7,
+  postsPerPage: 10,
   sortByDate: false,
-  showAbout: true,
+  showAbout: false,
   showArchive: true,
-  autoCollapsedNavBar: true, // The automatically collapsed navigation bar
+  autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: 'https://twitter.com/dario60271762',
   seo: {
