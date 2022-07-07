6b7f1967377cafa91acd047cec3c16b17e546cad
dario-github <zdclink@gmail.com>
2022-07-07 17:40:30 +0000

    [FEAT] Use different author

--- layouts/layout.js
+++ layouts/layout.js
@@ -37,15 +37,15 @@ const Layout = ({
         {frontMatter.type[0] !== 'Page' && (
           <nav className="flex mt-7 items-start text-gray-500 dark:text-gray-400">
             <div className="flex mb-4">
-              <a href={BLOG.socialLink || '#'} className="flex">
+              <a href={frontMatter.socialLink || '#'} className="flex">
                 <Image
-                  alt={BLOG.author}
+                  alt={frontMatter.author || BLOG.author}
                   width={24}
                   height={24}
                   src={`https://gravatar.com/avatar/${emailHash}`}
                   className="rounded-full"
                 />
-                <p className="ml-2 md:block">{BLOG.author}</p>
+                <p className="ml-2 md:block">{frontMatter.author || BLOG.author}</p>
               </a>
               <span className="block">&nbsp;/&nbsp;</span>
             </div>
